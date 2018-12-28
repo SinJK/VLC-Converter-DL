bc824040a11c7c1305e7394cbd01f9e360f9a611
SinJK <martin.roig@viacesi.fr>
2018-12-28 16:57:30 +0100

    Update VLC-Converter-youtube-DL.ps1

--- src/VLC-Converter-youtube-DL.ps1
+++ src/VLC-Converter-youtube-DL.ps1
@@ -2,7 +2,7 @@ Import-Module $PSScriptRoot\DiscogAPI.psm1
 
 <#
 .SYNOPSIS
-  This script simply download youtube with youtube-dl song or playlist and convert it into .mp3 with VLC converter.
+  This script download youtube video song or playlist, using youtube-dl, and convert it into .mp3 with VLC converter.
 .DESCRIPTION
   
 .PARAMETER <Parameter_Name>
